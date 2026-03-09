9c88d3486889d68457052e17c233f859c2f3e34b
Michael Naumov <mnaoumov@gmail.com>
2026-03-09 14:50:05 -0600

    refactor: add BasesView constructor and remove instances__ tracking
    Add explicit constructor with noop() to BasesView. Remove static
    instances__ fields from ButtonComponent and TextComponent as the __
    suffix convention and create__() spying make them unnecessary.

--- src/obsidian/ButtonComponent.ts
+++ src/obsidian/ButtonComponent.ts
@@ -8,14 +8,12 @@ import { strictMock } from '../internal/strict-mock.ts';
 import { BaseComponent } from './BaseComponent.ts';
 
 export class ButtonComponent extends BaseComponent {
-  public static instances__: ButtonComponent[] = [];
   public buttonEl: HTMLButtonElement;
   private clickHandler?: (evt: MouseEvent) => unknown;
 
   public constructor(containerEl: HTMLElement) {
     super();
     this.buttonEl = containerEl.createEl('button');
-    ButtonComponent.instances__.push(this);
   }
 
   public static create__(containerEl: HTMLElement): ButtonComponent {
